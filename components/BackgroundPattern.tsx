170c5a706a55c9a0bc0ad1c34dfacb5f33a4a088
devdilyorbek <dilyorbekdev@gmail.com>
2026-07-17 14:13:01 +0500

    NextPWA fix 2 backgound

--- components/BackgroundPattern.tsx
+++ components/BackgroundPattern.tsx
@@ -3,7 +3,7 @@ const BackgroundPattern = () => {
     <>
 
 
-      <div className="w-[500px] h-[500px] bg-primary/80  blur-[250px] opacity-75 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="w-[500px] max-md:hidden h-[500px] bg-primary/80  blur-[250px] opacity-75 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
     </>
   )
 }
